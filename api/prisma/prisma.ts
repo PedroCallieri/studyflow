1a81b3a16db1c524277fdc044c23874b0b23ccb0
PedroCallieri <pedroddrro@gmail.com>
2026-06-06 14:28:15 -0300

    organizei toda estrutura do backend

--- api/prisma/prisma.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "./generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-const connectionString = `postgresql://postgres:2910@localhost:5432/studyflow?schema=public`;
-
-const adapter = new PrismaPg({ connectionString });
-export const prisma = new PrismaClient({ adapter, log: ['query'] });
--- api/src/routes/UsuarioRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { usuarioController } from "../controller/UsuarioController";
-
-
-export const UsuarioRoutes = Router();
-
-UsuarioRoutes.get("/usuarios", (req,res) =>{
-    return usuarioController.usuarios(req, res);
-})
