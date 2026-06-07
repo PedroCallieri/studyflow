a95064e3fd5b79d1010a24aae55a6631ba78c57b
PedroCallieri <pedroddrro@gmail.com>
2026-06-07 16:22:42 -0300

    adiconei Service de sessões

--- src/service/SessaoService.js
+++ src/service/SessaoService.js
@@ -1,41 +1,30 @@
-import pool from '../db/connection.js'
+import sessaoRepository from '../repository/SessaoRepository.js'
 
 async function listarSessao() {
-    const result = await pool.query('SELECT * FROM sessao')
-    return result.rows;
+    const todasSessoes = await sessaoRepository.listarSessao()
+    return todasSessoes;
 }
 
 async function buscarSessaoId(id) {
-    const result = await pool.query(
-        'SELECT * FROM sessao WHERE id = $1', 
-        [id]
-    )
-    return result.rows[0];
+    const sessaoId = await sessaoRepository.buscarSessaoId(id) 
+    return sessaoId;
 }
 
 async function criarSessao(materia, tempoEstudado, dataRegistro, status, id_usuario) {
-    const result = await pool.query(
-        'INSERT INTO sessao (materia, tempoEstudado, dataRegistro, status, id_usuario) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-        [materia, tempoEstudado, dataRegistro, status, id_usuario]
-    );
-    return result.rows[0];
+  const sessao = await sessaoRepository.criarSessao(materia, tempoEstudado, dataRegistro, status, id_usuario)
+  return sessao;
 }
 
-async function atualizarSessao(id, materia, tempoEstudado, dataRegistro, status) {
-    const result = await pool.query(
-        'UPDATE sessao SET materia=$1, tempoEstudado=$2, dataRegistro=$3, status=$4 WHERE id=$5',
-        [materia, tempoEstudado, dataRegistro, status, id]
-    )
-    return result.rows[0];
+async function atualizarSessao(materia, tempoEstudado, dataRegistro, status, id) {
+    const sessao = await sessaoRepository.atualizarSessao(materia, tempoEstudado, dataRegistro, status, id)
+    return sessao;
 }
 
 async function deleteSessao(id) {
-    const result = await pool.query(
-        'DELETE FROM sessao WHERE id = $1',
-        [id]
-    )
-    return result.rows[0];
+    const sessao = await sessaoRepository.deleteSessao(id)
+    return sessao;
 }
+
 
 export default{ 
 listarSessao, 
