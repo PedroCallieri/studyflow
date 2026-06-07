fd619a9aa464c7fed9fc97f2f7dc492da8ec338f
PedroCallieri <pedroddrro@gmail.com>
2026-06-06 21:53:40 -0300

    adicionei Service de usuario

--- src/service/UsuarioService.js
+++ src/service/UsuarioService.js
@@ -1,1 +1,28 @@
-const pool = require('../db/connecti');
+import UsuarioRepository from '../repository/UsuarioRepository.js'
+
+async function listarUsuarios() {
+    const usuarios = await usuarioRepository.listarUsuarios()
+    return usuarios;
+}
+async function buscarUsuarioId(id) {
+    const usuarioId = await usuarioRepository.buscarUsuarioId(id) 
+    return usuarioId;
+}
+async function criarUsuario(nome, email, senha) {
+  const usuario = await usuarioRepository.criarUsuario(nome, email, senha)
+  return usuario;
+}
+async function atualizarUsuario(nome, email, senha, id) {
+    const usuario = await usuarioRepository.atualizarUsuario(nome, email, senha, id)
+    return usuario;
+}
+async function deleteUsuario(id) {
+    const usuario = await usuarioRepository.deleteUsuario(id)
+    return usuario;
+}
+export default{ 
+listarUsuarios, 
+buscarUsuarioId,
+criarUsuario,
+atualizarUsuario,
+deleteUsuario };
