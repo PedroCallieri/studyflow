5a789ea2ba59784738e2c887ded924421e458936
PedroCallieri <pedroddrro@gmail.com>
2026-06-06 22:19:40 -0300

    adicionei Controller de usuario

--- src/repository/UsuarioRepository.js
+++ src/repository/UsuarioRepository.js
@@ -4,10 +4,12 @@ async function listarUsuarios() {
     const result = await pool.query('SELECT * FROM usuario')
     return result.rows;
 }
+
 async function buscarUsuarioId(id) {
     const result = await pool.query('SELECT * FROM usuario WHERE usuario_id = $1', [id])
     return result.rows[0];
 }
+
 async function criarUsuario(nome, email, senha) {
     const result = await pool.query(
         'INSERT INTO usuario (nome, email, senha) VALUES ($1, $2, $3) RETURNING *',
@@ -15,6 +17,7 @@ async function criarUsuario(nome, email, senha) {
     );
     return result.rows[0];
 }
+
 async function atualizarUsuario(nome, email, senha, id) {
     const result = await pool.query(
         'UPDATE usuario SET nome=$1, email=$2, senha=$3 WHERE usuario_id=$4',
@@ -22,6 +25,7 @@ async function atualizarUsuario(nome, email, senha, id) {
     )
     return result.rows[0];
 }
+
 async function deleteUsuario(id) {
     const result = await pool.query(
         'DELETE FROM usuario WHERE usuario_id = $1',
@@ -29,6 +33,7 @@ async function deleteUsuario(id) {
     )
     return result.rows[0];
 }
+
 export default{ 
 listarUsuarios, 
 buscarUsuarioId,
